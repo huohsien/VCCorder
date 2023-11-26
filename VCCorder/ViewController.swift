8a3547c96618118d1a752262236b4589d78140df
Victor Jiang <huohsien@me.com>
2023-11-26 11:15:30 +0800

    Initial Commit

---
+++ VCCorder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VCCorder
+//
+//  Created by victor on 2023/11/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
